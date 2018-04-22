18c83fffa87dd159f10d30cfadcc2105c7785941
Shabab Ayub <shabab.ayub@shopify.com>
2018-04-22 15:38:59 -0400

    Fix bug for time encoding and add tests

--- tempo.go
+++ tempo.go
@@ -1,17 +1,29 @@
 package tempo
 
+import "time"
+
 type TimeSeries struct {
 	Opened Block
 	Closed []Block
 
-	TimeHeader       uint64
-	LatestTime       uint64
+	TimeHeader       int64
+	LatestTime       int64
 	LatestData       uint64
 	LatestDataXor    uint64
-	SecondLatestTime uint64
+	SecondLatestTime int64
 }
 
 type Block struct {
 	Length int
 	Stream []byte
+}
+
+// NewTimeSeries ....
+func NewTimeSeries(start time.Time) *TimeSeries {
+	ts := &TimeSeries{}
+
+	// epoch ms
+	ts.TimeHeader = start.Unix()
+
+	return ts
 }
